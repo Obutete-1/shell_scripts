7042c66d649f99ef179d7376bbc9f9d49e3dc7ca
Obutete-1 <gabrielobute1@gmail.com>
2025-06-11 13:29:54 +0000

    webdeploy and multios script

---
+++ remote_websetup/webdeploy.sh
@@ -0,0 +1,15 @@
+USR='devops'
+
+for host in `cat remhosts`
+do
+	echo
+	echo "##########################################"
+	echo "Connecting to $host"
+	echo "Pushing Script to $host"
+	scp multios_websetup $USR@$host:/tmp/
+	echo "Executing Script on $host"
+	ssh $USR@$host sudo /tmp/multios_websetup
+	ssh $USR@$host sudo rm -rf /tmp/multios_websetup
+	echo "##########################################"
+	echo
+done	
